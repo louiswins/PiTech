2b0a6e0b822225ceb2b986a55716b0b6f7f2bf03
Louis Wilson <louis.wilson@asu.edu>
2011-10-29 14:46:55 -0700

    Reorganization of GUI code.
    I put some of Samira's code in a more-easily-understandable order, and added a
    few basic Javadoc comments.

--- ControlTab.java
+++ ControlTab.java
@@ -2,62 +2,45 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.Font;
-
-
 import java.awt.event.*;
 import java.util.ArrayList;
 
+/**
+ * Controls the Session. Shows all the important information and data about the
+ * current run session.
+ *
+ * @version 0.2
+ */
 public class ControlTab extends JPanel{
 	private int timeCurrent, timeElapsed, speedCurrent, speedAverage, inclineCurrent, distanceCurrent, distanceTarget, caloriesCurrent, caloriesTarget;
+	/* Buttons */
 	private JButton quickStart_Resume, pause_Stop, reset, goal_Run_Start, speedUp, speedDown, inclineUp, inclineDown;
+	private JRadioButton[] radioButtonsGoalRun;
+	private ButtonGroup myButtonGroup;
+
+	/* Panels */
 	private JPanel panelMessage, panelInputOutput, panelOutputs, panelInputs;
 	private JPanel panelBasicFunc, panelGoalStart;
 	private JPanel panelGoalStartLeft, panelGoalStartRight, panelSpeedIncline;
-	
 	private JPanel panelTime, panelSpeed, panelIncline, panelDistance, panelCalories;
 	private JPanel panelDistanceRadio, panelDurationRadio, panelCaloriesRadio;
+
+	/* Labels */
 	private JLabel labelTimeCur, labelTimeElapsed, labelSpeedCur, labelSpeedAvg;
 	private JLabel labelTimeCurVal, labelTimeElapsedVal, labelSpeedCurVal, labelSpeedAvgVal;
 	private JLabel labelInclineCur, labelDistanceCur, labelDistanceTarg, labelCaloriesCur, labelCaloriesTarg;
 	private JLabel labelInclineCurVal, labelDistanceCurVal, labelDistanceTargVal, labelCaloriesCurVal, labelCaloriesTargVal;
 	private JLabel message, labelSpeed, labelIncline;
+
+	/* Misc */
 	private Border blackline;
 	private Font currentFont;
-	private ButtonGroup myButtonGroup;
 	private JTextField textFieldGoal1, textFieldGoal2, textFieldGoal3;
 
 	
-	private JRadioButton[] radioButtonsGoalRun;
-
-	
 	public ControlTab(){
-		panelInputOutput = new JPanel();
-		panelInputOutput.setLayout(new GridLayout(2,1));
 		
-		labelSpeed = new JLabel("Speed");
-		labelIncline = new JLabel("Incline");
-		textFieldGoal1 = new JTextField(30);
-		textFieldGoal2= new JTextField(30);
-		textFieldGoal3 = new JTextField(30);
-		myButtonGroup = new ButtonGroup();
-		blackline = BorderFactory.createLineBorder(Color.black);
-		panelSpeedIncline = new JPanel();
-		panelSpeedIncline.setLayout(new GridLayout(2,2));
-		
-		quickStart_Resume = new JButton("QuickStart / Resume");
-		pause_Stop = new JButton("Pause / Stop");
-		reset = new JButton("Reset");
-		goal_Run_Start = new JButton("Goal Run Start");
-		speedUp = new JButton("Speed Up");
-		speedDown = new JButton("Speed Down");
-		inclineUp = new JButton("Incline Up");
-		inclineDown = new JButton("Incline Down");
-		
-		message = new JLabel("Welcome to the treadmill machine");
-		message.setForeground(Color.blue);
-		currentFont = message.getFont();
-		message.setFont(new Font(currentFont.getFontName(), currentFont.getStyle(), 15));
-		
+		/* Set up the current variables. */
 		timeCurrent = 0; 
 		timeElapsed  = 0;
 		speedCurrent = 0;
@@ -68,6 +51,29 @@ public class ControlTab extends JPanel{
 		caloriesCurrent = 0;
 		caloriesTarget= 0;
 
+		/* Set up buttons */
+		quickStart_Resume = new JButton("QuickStart / Resume");
+		pause_Stop = new JButton("Pause / Stop");
+		reset = new JButton("Reset");
+		goal_Run_Start = new JButton("Goal Run Start");
+		speedUp = new JButton("Speed Up");
+		speedDown = new JButton("Speed Down");
+		inclineUp = new JButton("Incline Up");
+		inclineDown = new JButton("Incline Down");
+
+		radioButtonsGoalRun = new JRadioButton[3];
+		radioButtonsGoalRun[0] = new JRadioButton("distance", true);
+		radioButtonsGoalRun[1] = new JRadioButton("duration");
+		radioButtonsGoalRun[2] = new JRadioButton("calories");
+		myButtonGroup = new ButtonGroup();
+		myButtonGroup.add(radioButtonsGoalRun[0]);
+		myButtonGroup.add(radioButtonsGoalRun[1]);
+		myButtonGroup.add(radioButtonsGoalRun[2]);
+		
+		
+		/* Set up labels */
+		labelSpeed = new JLabel("Speed");
+		labelIncline = new JLabel("Incline");
 		labelTimeCur = new JLabel("Current Time"); 
 		labelTimeElapsed = new JLabel("Elapsed Time"); 
 		labelSpeedCur = new JLabel("Current Speed"); 
@@ -85,26 +91,80 @@ public class ControlTab extends JPanel{
 		labelDistanceCurVal = new JLabel("0 mi"); 
 		labelDistanceTargVal = new JLabel("0 mi"); 
 		labelCaloriesCurVal = new JLabel("0"); 
-		labelCaloriesTargVal = new JLabel("0"); 
-		
-		radioButtonsGoalRun = new JRadioButton[3];
-		radioButtonsGoalRun[0] = new JRadioButton("distance", true);
-		radioButtonsGoalRun[1] = new JRadioButton("duration");
-		radioButtonsGoalRun[2] = new JRadioButton("calories");
+		labelCaloriesTargVal = new JLabel("0");
 
-		myButtonGroup.add(radioButtonsGoalRun[0]);
-		myButtonGroup.add(radioButtonsGoalRun[1]);
-		myButtonGroup.add(radioButtonsGoalRun[2]);
+		message = new JLabel("Welcome to the treadmill machine");
+		message.setForeground(Color.blue);
+		currentFont = message.getFont();
+		message.setFont(new Font(currentFont.getFontName(), currentFont.getStyle(), 15));
+		blackline = BorderFactory.createLineBorder(Color.black);
 		
+		textFieldGoal1 = new JTextField(30);
+		textFieldGoal2= new JTextField(30);
+		textFieldGoal3 = new JTextField(30);
+
+
+		
+		/* Set up all the panels, top to bottom, left to right: */
+
+		/* Message */
+		panelMessage = new JPanel(); 
+		panelMessage.setBorder(blackline);
+		panelMessage.add(message);
+
+
+		/* Outputs */
+		panelTime = new JPanel(); 
+		panelTime.setLayout(new GridLayout(5,1));
+		panelTime.add(labelTimeCur);
+		panelTime.add(labelTimeCurVal);
+		panelTime.add(labelTimeElapsed);
+		panelTime.add(labelTimeElapsedVal);
+
+		panelSpeed = new JPanel(); 
+		panelSpeed.setLayout(new GridLayout(5,1));
+		panelSpeed.add(labelSpeedCur);
+		panelSpeed.add(labelSpeedCurVal);
+		panelSpeed.add(labelSpeedAvg);
+		panelSpeed.add(labelSpeedAvgVal);
+
+		panelIncline = new JPanel(); 
+		panelIncline.setLayout(new GridLayout(5,1));
+		panelIncline.add(labelInclineCur);
+		panelIncline.add(labelInclineCurVal);
+
+		panelDistance = new JPanel(); 
+		panelDistance.setLayout(new GridLayout(5,1));
+		panelDistance.add(labelDistanceCur);
+		panelDistance.add(labelDistanceCurVal);
+		panelDistance.add(labelDistanceTarg);
+		panelDistance.add(labelDistanceTargVal);
+		
+		panelCalories = new JPanel(); 
+		panelCalories.setLayout(new GridLayout(5,1));
+		panelCalories.add(labelCaloriesCur);
+		panelCalories.add(labelCaloriesCurVal);
+		panelCalories.add(labelCaloriesTarg);
+		panelCalories.add(labelCaloriesTargVal);
+
+		panelOutputs = new JPanel(); 
+		panelOutputs.setLayout(new GridLayout(1,5));
+		panelOutputs.add(panelTime);
+		panelOutputs.add(panelSpeed);
+		panelOutputs.add(panelIncline);
+		panelOutputs.add(panelDistance);
+		panelOutputs.add(panelCalories);
+		
+
+		/* Inputs */
+		/* Basic functions: left */
 		panelBasicFunc = new JPanel();
 		panelBasicFunc.setLayout(new GridLayout(3,1));
 		panelBasicFunc.add(quickStart_Resume);
 		panelBasicFunc.add(pause_Stop);
 		panelBasicFunc.add(reset);
 		
-		panelGoalStartLeft = new JPanel();
-		panelGoalStartRight = new JPanel();
-		
+		/* Goals & speed/incline controls: right */
 		panelDistanceRadio = new JPanel();
 		panelDistanceRadio.setLayout(new GridLayout(1,2));		
 		panelDistanceRadio.add(radioButtonsGoalRun[0]);
@@ -117,8 +177,16 @@ public class ControlTab extends JPanel{
 		panelCaloriesRadio.setLayout(new GridLayout(1,2));
 		panelCaloriesRadio.add(radioButtonsGoalRun[2]);
 		panelCaloriesRadio.add(textFieldGoal3);
-
 		
+		panelSpeedIncline = new JPanel();
+		panelSpeedIncline.setLayout(new GridLayout(2,2));
+		panelSpeedIncline.add(speedUp);
+		panelSpeedIncline.add(inclineUp);
+		panelSpeedIncline.add(speedDown);
+		panelSpeedIncline.add(inclineDown);
+//		panelSpeedIncline.add(labelSpeed);
+//		panelSpeedIncline.add(labelIncline);
+
 		panelGoalStart = new JPanel();
 		panelGoalStart.setLayout(new GridLayout(5,1));
 		panelGoalStart.add(goal_Run_Start);
@@ -126,83 +194,35 @@ public class ControlTab extends JPanel{
 		panelGoalStart.add(panelDurationRadio);
 		panelGoalStart.add(panelCaloriesRadio);
 		panelGoalStart.add(panelSpeedIncline);
-		
-		panelSpeedIncline.add(speedUp);
-		panelSpeedIncline.add(inclineUp);
-		panelSpeedIncline.add(speedDown);
-		panelSpeedIncline.add(inclineDown);
-//		panelSpeedIncline.add(labelSpeed);
-//		panelSpeedIncline.add(labelIncline);
-		
-		panelMessage = new JPanel(); 
-		panelOutputs = new JPanel(); 
+
+		/* All inputs */
 		panelInputs = new JPanel(); 
-		panelTime = new JPanel(); 
-		panelSpeed = new JPanel(); 
-		panelIncline = new JPanel(); 
-		panelDistance = new JPanel(); 
-		panelCalories = new JPanel(); 
-		
-		
-		panelMessage.setBorder(blackline);
-		panelMessage.add(message);
-		
-		panelTime.setLayout(new GridLayout(5,1));
-		panelSpeed.setLayout(new GridLayout(5,1));
-		panelIncline.setLayout(new GridLayout(5,1));
-		panelDistance.setLayout(new GridLayout(5,1));
-		panelCalories.setLayout(new GridLayout(5,1));
-		panelTime.add(labelTimeCur);
-		panelTime.add(labelTimeCurVal);
-		panelTime.add(labelTimeElapsed);
-		panelTime.add(labelTimeElapsedVal);
-		
-		
-		panelSpeed.add(labelSpeedCur);
-		panelSpeed.add(labelSpeedCurVal);
-		panelSpeed.add(labelSpeedAvg);
-		panelSpeed.add(labelSpeedAvgVal);
-		
-		panelIncline.add(labelInclineCur);
-		panelIncline.add(labelInclineCurVal);
-		
-		panelDistance.add(labelDistanceCur);
-		panelDistance.add(labelDistanceCurVal);
-		panelDistance.add(labelDistanceTarg);
-		panelDistance.add(labelDistanceTargVal);
-		
-		panelCalories.add(labelCaloriesCur);
-		panelCalories.add(labelCaloriesCurVal);
-		panelCalories.add(labelCaloriesTarg);
-		panelCalories.add(labelCaloriesTargVal);
-		
-		panelOutputs.setLayout(new GridLayout(1,5));
-		panelOutputs.add(panelTime);
-		panelOutputs.add(panelSpeed);
-		panelOutputs.add(panelIncline);
-		panelOutputs.add(panelDistance);
-		panelOutputs.add(panelCalories);
-		
 		panelInputs.setLayout(new GridLayout(1,2));
 		panelInputs.add(panelBasicFunc);
 		panelInputs.add(panelGoalStart);
 		
+		
+		/* Bring it all together */
+		panelInputOutput = new JPanel();
+		panelInputOutput.setLayout(new GridLayout(2,1));
 		panelInputOutput.add(panelOutputs);
 		panelInputOutput.add(panelInputs);
-		//setLayout(new GridLayout(3,1));
-		setLayout(new BorderLayout());
-		//add(panelMessage);
-		//add(panelInputOutput);
-		//add(panelOutputs);
-		//add(panelInputs);
 		
-		//setLayout(new BorderLayout());
+		setLayout(new BorderLayout());
 		add(BorderLayout.NORTH, panelMessage);
 		add(BorderLayout.CENTER, panelInputOutput);
-		//add(BorderLayout.CENTER, panelOutputs);
-		//add(BorderLayout.SOUTH, panelInputs);
+		
+		
+
+
+		/* Unused? */
+		panelGoalStartLeft = new JPanel();
+		panelGoalStartRight = new JPanel();
 	}
 
+
+
+	/** Listener for button events */
 	private class ButtonListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent event)
--- DisplayInterface.java
+++ DisplayInterface.java
@@ -1,45 +1,34 @@
-// Assignment #: 6
-//         Name:
-//    StudentID:
-//      Lecture:
-//  Description: The Assignment 6 class creates a Tabbed Pane with
-//               two tabs, one for Account creation and one for
-//               Account transfer. and adds it as its Applet content
-//               and also sets its size.
-
 import javax.swing.*;
 import java.util.*;
 
+/**
+ * Main interface of the program. This class just sets up the window and passes
+ * control to the active tab.
+ *
+ * @version 0.2
+ */
 public class DisplayInterface extends JApplet
 {
 
-	private int APPLET_WIDTH = 975, APPLET_HEIGHT = 525;
+	/** Default width of applet. */
+	private int APPLET_WIDTH = 975;
+	/** Default height of applet. */
+	private int APPLET_HEIGHT = 525;
 
 	private JTabbedPane tPane;
 	private ControlTab myTreadmill;
-	//private TransferPanel transferPanel;
-	// private Vector accountList;
 
-	//The method init initializes the Applet with a Pane with two tabs
+	/**
+	 * Initializes the Applet with a Pane of two tabs.
+	 */
 	public void init()
 	{
-		//list of accounts to be used in every panel
-		//accountList = new Vector();
-
-		//register panel uses the list of accounts
-		//transferPanel = new TransferPanel(accountList);
-
-		//    createPanel = new CreatePanel(accountList, transferPanel);
-
 		//create a tabbed pane with two tabs
 		myTreadmill = new ControlTab();
 		tPane = new JTabbedPane();
 		tPane.addTab("Treadmill", myTreadmill);
-		// tPane.addTab("Account transfer", transferPanel);
 
 		getContentPane().add(tPane);
 		setSize (APPLET_WIDTH, APPLET_HEIGHT); //set Applet size
 	}
 }
-
-
